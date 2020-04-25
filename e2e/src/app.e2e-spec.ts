558110d1c5cc42829ecd4888c3a40e076d100265
prakashalamanda <50372054+prakashalamanda@users.noreply.github.com>
2020-04-25 01:56:19 +0100

    minor fixes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('hello-buddy app is running!');
+    expect(page.getTitleText()).toEqual('help-buddy app is running!');
   });
 
   afterEach(async () => {
